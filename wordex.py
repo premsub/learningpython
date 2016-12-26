c4036be9fe323af36ebf9fdadecf0576ba02d59e
premsub <premsub2@gmail.com>
2016-12-26 20:46:56 +0000

    adding word manipulation files

---
+++ wordex.py
@@ -0,0 +1,7 @@
+from wordman import *
+words=split_words("How are you doing")
+print "length=%d" % len(words)
+i=0
+while i < len(words):
+	print words[i]
+	i=i+1
---
+++ wordman.py
@@ -0,0 +1,5 @@
+
+def split_words(sentence):
+	return sentence.split(' ')
+def sort_words(words_input):
+	return sorted(words_input)
